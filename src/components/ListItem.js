ebf74406d45f30ab576739d87a97826c73b653a7
oguzergul <erguloguz@hotmail.com>
2022-02-20 19:13:19 +0300

    fix button class

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,14 +1,12 @@
 import React from "react";
 import {useDispatch} from "react-redux";
 import {TrashIcon, CheckCircleIcon, MinusCircleIcon} from "@heroicons/react/solid";
-import {useSelector} from "react-redux";
 
 import {updateTodo} from "../store/todoSlice";
 
-import {Input} from "../components";
 
-export const ListItem = ({data,onDelete,isCompleted}) => {
-    const { todo } = data;
+export const ListItem = ({data,onDelete}) => {
+    const { todo, completed } = data;
     const dispatch = useDispatch();
 
     const checkTodo = () => {
@@ -21,7 +19,7 @@ export const ListItem = ({data,onDelete,isCompleted}) => {
     return (
         <div className="flex flex-row justify-between items-center bg-white shadow-lg p-4 rounded-lg mb-4">
            <div className="flex flex-row items-center overflow-hidden ">
-               {isCompleted
+               {completed
                    ? <CheckCircleIcon onClick={() => checkTodo()} height={20} width={20} fill="green"/>
                    : <MinusCircleIcon onClick={() => checkTodo()}height={20} width={20} fill="gray" />
                }
@@ -29,9 +27,9 @@ export const ListItem = ({data,onDelete,isCompleted}) => {
            </div>
             <div className="flex flex-row">
 
-                <button onClick={onDelete} className=" h-8 w-8 rounded-md hover:bg-gray-100 items-center flex justify-center">
+                <div onClick={onDelete} className="cursor-pointer h-8 w-8 rounded-md hover:bg-gray-100 items-center flex justify-center">
                     <TrashIcon height={20} width={20} fill="#EF4444"/>
-                </button>
+                </div>
             </div>
         </div>
     )
--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 let axiosInstance = axios.create({
-    baseURL: process.env.BASE_URL,
+    baseURL: "https://oguz-todo.herokuapp.com/",
     headers: { "Content-Type": "application/json; charset=UTF-8" },
 });
 
