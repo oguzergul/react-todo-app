01bc357ac42f2a0f031270f31479ad6797db966d
oguzergul <erguloguz@hotmail.com>
2022-02-20 19:32:54 +0300

    fix production build bug

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-import "./Button.css"
 
 export const Button = ({children,onClick}) => {
     return (
         <button
+            className="ml-2 inline-flex justify-center items-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-black text-base font-medium text-white hover:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-black sm:ml-3 sm:w-auto sm:text-sm"
             onClick={onClick}>
             {children}
         </button>
